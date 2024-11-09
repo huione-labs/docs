86d073352c754f5a334a715360d0d7ee995dbe86
你的用户名 <mangosago.liu@gmail.com>
2024-11-10 02:12:26 +0700

    Update the theme

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -50,4 +50,3 @@ export { default as TwitterX } from './twitterx.svg';
 
 
 
-
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customRed: '#ED0000',
+        'tahiti': '#ED0000',
+      },
+    },
+  },
+  plugins: [],
+}
+
