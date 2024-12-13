bc4b2537ab66b291e4a7b9ca33bc487b61e0473d
你的用户名 <mangosago.liu@gmail.com>
2024-12-13 18:21:35 +0700

    #

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-const withNextra = require('nextra')({
+const nextra = require('nextra');
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  webpack(config) {
+    const allowedSvgRegex = /\.svg$/
+
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    )
+    fileLoaderRule.exclude = allowedSvgRegex
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ['@svgr/webpack']
+    })
+    return config
+  }
+})
