24c701799b47f867734c77cc7f4d4f6f5d5efa28
你的用户名 <mangosago.liu@gmail.com>
2025-02-13 20:06:17 +0700

    new media imgs

---
+++ src/next-seo.config.ts
@@ -0,0 +1,25 @@
+export default {
+  title: "Xone Docs | Let’s Embrace The Future Together!",
+  description: "The decentralized Layer-1 blockchain platform based on the Ethereum protocol uses leading behavioral value incentives (BVI) to ensure that every interaction of every user participating in Xone will create value, and every contribution will be rewarded.",
+  openGraph: {
+    type: "xone_docs",
+    locale: "zh_en",
+    url: "https://docs.xone.plus",
+    title: "Xone Docs | Let’s Embrace The Future Together!",
+    description: "The decentralized Layer-1 blockchain platform based on the Ethereum protocol uses leading behavioral value incentives (BVI) to ensure that every interaction of every user participating in Xone will create value, and every contribution will be rewarded.",
+    images: [
+      {
+        url: "/images/summary_large_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Xone Docs Preview",
+      },
+    ],
+    site_name: "Xone Docs",
+  },
+  twitter: {
+    handle: "@xone_chain",
+    site: "www.xone.org",
+    cardType: "summary_large_image",
+  },
+};
